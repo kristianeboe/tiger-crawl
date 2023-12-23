7414aeb74d1dedffebde088c5ecddcd4dc8ca066
Kristian Elset Bø <kristian.e.boe@gmail.com>
2023-12-23 21:53:06 +0100

    changed to prisma

---
+++ src/app/_components/Nav.tsx
@@ -0,0 +1,46 @@
+import { MobileNav } from "./MobileNav";
+
+export const navigation = [
+  { name: "Product", href: "#" },
+  { name: "Features", href: "#" },
+  { name: "Resources", href: "#" },
+  { name: "Company", href: "#" },
+];
+
+export function Nav() {
+  return (
+    <nav
+      className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+      aria-label="Global"
+    >
+      <div className="flex lg:flex-1">
+        <a href="#" className="-m-1.5 p-1.5">
+          <span className="sr-only">Your Company</span>
+          <img
+            className="h-8 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt=""
+          />
+        </a>
+      </div>
+
+      <MobileNav />
+      <div className="hidden lg:flex lg:gap-x-12">
+        {navigation.map((item) => (
+          <a
+            key={item.name}
+            href={item.href}
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
+            {item.name}
+          </a>
+        ))}
+      </div>
+      <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+          Log in <span aria-hidden="true">&rarr;</span>
+        </a>
+      </div>
+    </nav>
+  );
+}
